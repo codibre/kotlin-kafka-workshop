7c5c921140978cc2da9f574ba30c081e7ed2a07b
Gustavo Andrade Ferreira <gustavo.andrade@maxmilhas.com.br>
2023-05-18 15:34:55 -0300

    Adicionando maven central ao build root

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("jvm") version "1.8.21"
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.6")
 }
